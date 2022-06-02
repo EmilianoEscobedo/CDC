39b6c92087059976463cbf933f7a38d753303da1
Laingard <emilianoescobedo9@gmail.com>
2022-06-02 16:00:48 -0300

    Built js functionality on service buttons, added header info

--- moves.js
+++ moves.js
@@ -2,6 +2,7 @@ function change(actual){
     if (actual=='-'){
         actual='+'
     }else {actual='-'}
+    console.log('ajsj')
     return actual
     
  };
@@ -11,6 +12,8 @@ const service1 = document.getElementById('service1');
 const service2 = document.getElementById('service2');
 const service3 = document.getElementById('service3');
 const service4 = document.getElementById('service4');
+const service5 = document.getElementById('service5');
+const service6 = document.getElementById('service6');
 
 services.addEventListener('click', e =>{
     if(e.target.classList.contains('btn1')){
@@ -24,5 +27,11 @@ services.addEventListener('click', e =>{
     }
     if(e.target.classList.contains('btn4')){
         service4.innerHTML=change(service4.innerHTML);
+    }
+    if(e.target.classList.contains('btn5')){
+        service5.innerHTML=change(service5.innerHTML);
     };
+    if(e.target.classList.contains('btn6')){
+        service6.innerHTML=change(service6.innerHTML);
+    };;
 });
