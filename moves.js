67d37701bd36a698d6511c19354f3b71d68706e2
Laingard <emilianoescobedo9@gmail.com>
2022-07-17 02:35:15 -0300

    optmized service buttons

--- moves.js
+++ moves.js
@@ -1,40 +1,13 @@
 // ----------- Services ----------- 
 
-function change(actual){
-    if (actual=='-'){
-        actual='+'
-    }else {actual='-'}
-    console.log('ajsj')
-    return actual
- };
+const services = document.getElementsByClassName('services');
 
-const services = document.getElementById('services');
-const service1 = document.getElementById('service1');
-const service2 = document.getElementById('service2');
-const service3 = document.getElementById('service3');
-const service4 = document.getElementById('service4');
-const service5 = document.getElementById('service5');
-const service6 = document.getElementById('service6');
-
-services.addEventListener('click', e =>{
-    if(e.target.classList.contains('btn1')){
-        service1.innerHTML=change(service1.innerHTML);
-    }
-    if(e.target.classList.contains('btn2')){
-        service2.innerHTML=change(service2.innerHTML);
-    }
-    if(e.target.classList.contains('btn3')){
-        service3.innerHTML=change(service3.innerHTML);
-    }
-    if(e.target.classList.contains('btn4')){
-        service4.innerHTML=change(service4.innerHTML);
-    }
-    if(e.target.classList.contains('btn5')){
-        service5.innerHTML=change(service5.innerHTML);
-    };
-    if(e.target.classList.contains('btn6')){
-        service6.innerHTML=change(service6.innerHTML);
-    };;
+let service = Array.from(services);
+service.forEach(el => {
+    el.addEventListener('click', () => {
+    let actual = el.children[2].innerHTML;
+    (actual=='+') ? (el.children[2].innerHTML='-', el.children[1].style.fontWeight='500') : (el.children[2].innerHTML='+', el.children[1].style.fontWeight='normal');
+    });
 });
 
 // ----------- Counter ----------- 
@@ -46,21 +19,23 @@ addEventListener('DOMContentLoaded', () => {
         for (const counter of counters) {
             let max_quantity = +counter.dataset.total,
                 actual_value = +counter.innerText
-                let time = setInterval(()=> {
+                if (actual_value != max_quantity){
+                    let time = setInterval(()=> {
                     counter.innerText = (actual_value +=1);
                     if (actual_value === max_quantity){
                         clearInterval(time)
                         counter.style.color='#BBA14F';
                     }
                 },150);
-            }
+            }}
     }
+    //API intersectionObserver
     const showCounters = elements => {
         elements.forEach(element => {
             if(element.isIntersecting) {
                 element.target.classList.add('animate');
                 element.target.classList.remove('dontShow');
-                setTimeout(animateCounters,300)
+                setTimeout(animateCounters,300);
             }
         });
     }
@@ -69,6 +44,7 @@ addEventListener('DOMContentLoaded', () => {
     })
     const elementHTML = document.getElementById('counters')
     observer.observe(elementHTML)
+    
 
 })
 
