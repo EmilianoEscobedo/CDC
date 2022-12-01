24f50f9dec71eff1fb3ed3d6f700ab6c18af349a
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-01 16:30:31 -0300

    Built structure of Who We Are component

---
+++ src/components/WhoWeAre.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const WhoWeAre = () => {
+  return (
+    <>
+        <div className='w-100 my-5 d-flex justify-content-center align-items-center'>
+            <div className="container">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugiat laudantium omnis! Eum perspiciatis sed quo doloribus? Modi obcaecati dicta ex numquam ipsa cupiditate deserunt, culpa velit, harum beatae adipisci. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod voluptatibus corporis autem in, magnam voluptates natus alias saepe quia reprehenderit, deleniti soluta quidem rerum doloremque? Numquam quis distinctio sit voluptas.
+            </p>
+            </div>
+        </div>
+    </>
+  )
+}
