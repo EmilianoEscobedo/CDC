475263f1b9efabbbc7f086d0f69c5a41f333c0dd
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-01 16:32:35 -0300

    Adedded new components to home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,16 @@
-import {Slider} from '../components/Slider'
-import {Services} from '../components/Services'
-import {CounterServices} from '../components/CounterServices'
-
-import React from 'react'
-
+import { Slider } from '../components/Slider'
+import { Services } from '../components/Services'
+import { WhoWeAre } from '../components/WhoWeAre'
+import { CounterServices } from '../components/CounterServices'
+import { Contact } from '../components/Contact'
 export const Home = () => {
   return (
     <>
         <Slider />
         <Services />
+        <WhoWeAre />
         <CounterServices />
+        <Contact />
     </>
   )
 }
