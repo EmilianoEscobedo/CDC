862318a1e5536c97078fb9afad32278e7da2cc45
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-01 16:08:48 -0300

    Built routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom";
+import DataContextProvider from './contexts/DataContext'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
