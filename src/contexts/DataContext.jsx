c42e5c4f0fe339644ef19d864967c2decae8cd5c
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-01 16:05:54 -0300

    built db context

---
+++ src/contexts/DataContext.jsx
@@ -0,0 +1,51 @@
+import {createContext, 
+    useContext, 
+    useState, 
+    useRef
+  } from "react";
+  import {useCollectionData} from "react-firebase-hooks/firestore";
+  import {app} from "../firebase/credentials";
+  import {
+    collection,
+    orderBy,
+    limit,
+    query,
+    setDoc,
+    doc,
+    serverTimestamp,
+    addDoc,
+    getFirestore
+  } from "firebase/firestore";
+  
+  
+  export const dataContext = createContext();
+  export const useDataContext = () => {
+    const context = useContext(dataContext);
+    return context;
+  };
+  export default function DataContextProvider(props) {
+    const {children} = props;
+    const db = getFirestore(app);
+    
+    //Slider data
+    const sliderRef = collection(db, 'assets/images/Slider');
+    const querySlider = query(sliderRef);
+    const [imgSlider] = useCollectionData(querySlider);
+
+    //Services data
+    const servicesRef = collection(db, 'assets/services/services');
+    const queryServices = query(servicesRef);
+    const [servicesData] = useCollectionData(queryServices);
+
+    return (
+      <dataContext.Provider
+        value={{
+          imgSlider,
+          servicesData,
+        }}
+      >
+        {children}
+      </dataContext.Provider>
+    );
+  }
+  
