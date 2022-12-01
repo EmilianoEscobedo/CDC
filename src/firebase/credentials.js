66e785ad0ab6942aac147d53b4c81a39694c7601
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-01 16:06:23 -0300

    firebase connection

---
+++ src/firebase/credentials.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0jSKqQxWTgd7CsPr3XXe1--381ztmQ4Q",
+  authDomain: "cdc-construccion.firebaseapp.com",
+  projectId: "cdc-construccion",
+  storageBucket: "cdc-construccion.appspot.com",
+  messagingSenderId: "278810386714",
+  appId: "1:278810386714:web:58e3ec935494abaf770ca3"
+};
+
+export const app = initializeApp(firebaseConfig);
