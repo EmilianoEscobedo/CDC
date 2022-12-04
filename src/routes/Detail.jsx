10d717b1b3efcbdac1bc8c71a16e064a366d32b7
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-04 00:26:23 -0300

    Improved view detail/home routes

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,15 +1,22 @@
 import { useDataContext } from '../contexts/DataContext';
 import { useParams } from 'react-router-dom';
+import '../assets/css/detail.css'
 
 export const Detail = () => {
   const {serviceId} = useParams();
   const {servicesData} = useDataContext();
   const service = servicesData.find(e => e.title === serviceId);
   return (
-    <div>
-      <h1>{service.title}</h1>
-      <img src={service.image} alt="" />
-      <p>{service.description}</p>
+    <div className='row detail__container'>
+      <div className='col-sm-1'></div>
+      <div className='col-sm-3 text-center mb-4'>
+        <img className='detail__image' src={service.image} alt='service image' />
+      </div>
+      <div className='col-sm-7'>
+        <h1 className='detail__title'>{service.title}</h1>
+        <p className='detail__text'>{service.description}</p>
+      </div>
+      <div className='col-sm-1'></div>
     </div>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,7 @@ import { Services } from '../components/Services'
 import { WhoWeAre } from '../components/WhoWeAre'
 import { CounterServices } from '../components/CounterServices'
 import { Contact } from '../components/Contact'
+
 export const Home = () => {
   return (
     <>
