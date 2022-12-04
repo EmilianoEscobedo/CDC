0c9140cc880ac9c44f7e221029271c5fb4209a1c
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-04 01:38:56 -0300

    resized images

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,14 +7,14 @@ export const Slider = () => {
     <div className="slider__container">
       <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={imgSlider && imgSlider[0].image} className="img-fluid w-100" alt="image slider 1" />
+            <div className="carousel-item active ">
+              <img src={imgSlider && imgSlider[0].image} className="slider__img img-fluid" alt="image slider 1" />
             </div>
             <div className="carousel-item">
-              <img src={imgSlider && imgSlider[1].image} className="img-fluid w-100" alt="image slider 2" />
+              <img src={imgSlider && imgSlider[1].image} className="slider__img img-fluid" alt="image slider 2" />
             </div>
             <div className="carousel-item">
-              <img src={imgSlider && imgSlider[2].image} className="img-fluid w-100" alt="image slider 3" />
+              <img src={imgSlider && imgSlider[2].image} className="slider__img img-fluid" alt="image slider 3" />
             </div>
           </div>
           <h1 className="slider__title fw-bold">La forma inteligente <br/> de construir.</h1>
